3a4ac995333768f601af12aa24e5f5f4d831d4cb
Isaac Bedard <ibedard@renovo1.com>
2019-09-28 17:10:03 -0400

    Handle web sockets
    Right now, users can subscribe to rooms to receive notifications about new messages. We're considering adding more functionality, but don't be surprised if nothing else happens - cause sockets are complicated.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const uuidv4 = require('uuid/v4');
+
 const app = express();
+const ws = require('express-ws')(app);
 const port = 3000;
 app.use(express.json());
 
 const serverName = 'Javascript Server';
 const adminPassword = 'Let me in!';
 const rooms = {};
+const activeSockets = [];
 
 app.post('/createRoom', (req, res) => {
 	if (req.body.adminPassword !== adminPassword) {
@@ -27,6 +30,7 @@ app.post('/createRoom', (req, res) => {
 	newRoom.name = req.body.name;
 	newRoom.description = req.body.description;
 	newRoom.messages = [];
+	newRoom.subscribedSocketIds = [];
 
 	if (!!req.body.accessPassword) {
 		newRoom.accessPassword = req.body.accessPassword;
@@ -56,6 +60,10 @@ app.delete('/room/:roomId', (req, res) => {
 		return res.status(404).send({ successful: false, errMsg: 'Room not found!'});
 	}
 
+	rooms[req.params.roomId].subscribedSocketIds.forEach(socketId => {
+		unsubscribeSocket(socketId, req.params.roomId);
+	});
+
 	delete rooms[req.params.roomId];
 
 	return res.send({successful: true})
@@ -76,11 +84,21 @@ app.post('/room/:roomId/message', (req, res) => {
 	}
 
 	const messageId = uuidv4();
-	room.messages.push({
+	const message = {
 		id: messageId,
 		sender: req.body.user,
 		message: req.body.message,
 		timeStamp: Math.floor(Date.now() / 1000),
+	};
+	room.messages.push(message);
+	room.subscribedSocketIds.forEach(socketId => {
+		try {
+			const socket = activeSockets.find(socket => socket.id === socketId).socket;
+			socket.send(JSON.stringify({update: {roomId: room.id, message}}));
+		} catch(e) {
+			console.log('Something went wrong notifying a subscriber!');
+			console.log(e);
+		}
 	});
 
 	res.send({successful: true, messageId: messageId});
@@ -114,5 +132,63 @@ app.get('/room/:roomId', (req, res) => {
 
 	res.send({successful: true, name: room.name, description: room.description, messages: messages});
 });
+
+
+app.ws('/', (socket, req) => {
+	const roomSubscriptions = [];
+	const socketId = uuidv4();
+	activeSockets.push({id: socketId, roomSubscriptions: roomSubscriptions, socket: socket});
+	socket.on('message', (args) => {
+		args = JSON.parse(args);
+		if (!!args.subscribe) {
+			const roomId = args.subscribe.roomId;
+			const accessPassword = args.subscribe.accessPassword;
+
+			const room = rooms[roomId];
+			if (!room) {
+				return socket.send(JSON.stringify({error: 'Room not found!'}));
+			}
+
+			if (!!room.accessPassword && room.accessPassword !== accessPassword) {
+				return socket.send(JSON.stringify({error: 'Invalid password for room ' + room.name}));
+			}
+
+			room.subscribedSocketIds.push(socketId);
+			roomSubscriptions.push(room.id);
+		}
+		if (!!args.unsubscribe) {
+			unsubscribeSocket(socket, args.unsubscribe.roomId);
+		}
+	});
+	socket.onclose(() => {
+		roomSubscriptions.forEach((roomSubscription) => {
+			unsubscribeSocket(socketId, roomSubscription);
+		});
+		const activeSocketIndex = activeSockets.findIndex(x => x.id === socketId);
+		activeSockets.splice(activeSocketIndex, 1);
+	});
+});
+
+function unsubscribeSocket(socketId, roomId) {
+	const socket = activeSockets.find(x => x.id === socketId);
+	if (!socket) {
+		return;
+	}
+
+	const room = rooms[roomId];
+	if (!room) {
+		return;
+	}
+
+	const subscriptionIndex = room.subscribedSocketIds.findIndex(x => x === socketId);
+	if (subscriptionIndex != -1) {
+		room.subscribedSocketIds.splice(subscriptionIndex, 1);
+	}
+
+	const roomIndex = socket.roomSubscriptions.findIndex(roomSubscription => roomSubscription === roomId);
+	if (roomIndex != -1) {
+		socket.roomSubscriptions.splice(roomIndex, 1);
+	}
+}
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
