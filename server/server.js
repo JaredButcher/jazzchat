95b13e345692a1c7f8faf8d155e09d08cdede0b2
Isaac Bedard <ibedard@renovo1.com>
2019-09-28 23:34:25 -0400

    Use MongoDB to store rooms
    This was very, very hard. I went down a lot of dead ends at first. I don't even remember everything I did to get this working.
    
    I regression tested everything, and everything seems to be in working order.
    
    If anyone's actually reading this commit, you might notice that we're not actually saving the messages yet. This is because they were already wrapped in the room, and it was hard enough getting rooms to work in the first place. I plan on treating messages as a separate entity... soon-ish. Probably next commit.
    
    Also, I renamed the "id" property on the room to "roomId". I was confused, I thought this would get mongo to magically work. It did not - it actually just got worse. (I wish I was using typescript!)

--- server/server.js
+++ server/server.js
@@ -8,9 +8,94 @@ app.use(express.json());
 let port;
 let serverName;
 let adminPassword;
+let connectionString;
+const dbName = 'JazzChat';
 
-const rooms = [];
 const activeSockets = [];
+const roomWatchers = {};
+
+const MongoClient = require('mongodb').MongoClient;
+const getDatabase = (cb) => {
+	const client = new MongoClient(connectionString);
+	client.connect(function(err) {
+		if (err) {
+			console.log(err);
+		}
+		const db = client.db(dbName);
+
+		cb(db);
+
+		client.close();
+	});
+}
+
+const countRooms = (cb) => {
+	getDatabase((db) => {
+		const collection = db.collection('rooms');
+
+		collection.count({}, (err, rooms) => {
+			if (err) {
+				console.log(err);
+			}
+
+			cb(rooms);
+		});
+	});
+}
+
+const getRooms = (cb) => {
+	getDatabase((db) => {
+		const collection = db.collection('rooms');
+
+		collection.find({}).toArray(function(err, rooms) {
+			if (err) {
+				console.log(err);
+			}
+
+			cb(rooms);
+		});
+	});
+}
+
+const getRoom = (roomId, cb) => {
+	getDatabase((db) => {
+		const collection = db.collection('rooms');
+
+		console.log(roomId);
+
+		collection.findOne({roomId: Number(roomId)}, (err, room) => {
+			if (err) {
+				console.log(err);
+			}
+
+			cb(room);
+		});
+	});
+}
+
+const addRoom = (room, cb) => {
+	getDatabase((db) => {
+		const collection = db.collection('rooms');
+
+		collection.insertOne(room, cb);
+	});
+}
+
+const updateRoom = (roomId, newRoom, cb) => {
+	getDatabase(db => {
+		const collection = db.collection('rooms');
+
+		collection.updateOne({ roomId : Number(roomId) }, { $set: newRoom }, cb);
+	});
+}
+
+const deleteRoom = (roomId, cb) => {
+	getDatabase(db => {
+		const collection = db.collection('rooms');
+
+		collection.deleteOne({ roomId : Number(roomId) }, cb);
+	});
+}
 
 app.post('/createRoom', (req, res) => {
 	console.log('received request to create room');
@@ -30,34 +115,37 @@ app.post('/createRoom', (req, res) => {
 		return res.status(422).send({ successful: false, errMsg: 'Missing description!' });
 	}
 
-	const newRoom = {};
+	countRooms(roomCount => {
+		const newRoom = {};
 
-	newRoom.id = rooms.length;
-	newRoom.name = req.body.name;
-	newRoom.description = req.body.description;
-	newRoom.messages = [];
-	newRoom.subscribedSocketIds = [];
+		newRoom.roomId = roomCount;
+		newRoom.name = req.body.name;
+		newRoom.description = req.body.description;
+		newRoom.messages = [];
 
-	if (!!req.body.accessPassword) {
-		newRoom.accessPassword = req.body.accessPassword;
-	}
+		if (!!req.body.accessPassword) {
+			newRoom.accessPassword = req.body.accessPassword;
+		}
 
-	rooms.push(newRoom);
-
-	res.send({successful: true, newRoomId: newRoom.id});
-	console.log(`user created a new chatroom ${newRoom.name}`);
+		addRoom(newRoom, () => {
+			res.send({successful: true, newRoomId: newRoom.roomId});
+			console.log(`user created a new chatroom ${newRoom.name}`);
+		});
+	});
 });
 
 app.get('/server', (req, res) => {
 	console.log(`user requested a list of rooms`);
 
-	const parsedRooms = [];
-	for (roomId in rooms) {
-		const room = rooms[roomId];
-		parsedRooms.push({ id: room.id, name: room.name, description: room.description, hasPassword: !!room.accessPassword }); 
-	};
+	getRooms(rooms => {
+		const parsedRooms = [];
+		for (roomId in rooms) {
+			const room = rooms[roomId];
+			parsedRooms.push({ id: room.roomId, name: room.name, description: room.description, hasPassword: !!room.accessPassword }); 
+		};
 
-	res.send({ name: serverName, rooms: parsedRooms});
+		res.send({ name: serverName, rooms: parsedRooms});
+	});
 });
 
 app.delete('/room/:roomId', (req, res) => {
@@ -66,18 +154,32 @@ app.delete('/room/:roomId', (req, res) => {
 		return res.status(401).send({ successful: false, errMsg: 'Invalid adminPassword!'});
 	}
 
-	if (!rooms[req.params.roomId]) {
-		return res.status(404).send({ successful: false, errMsg: 'Room not found!'});
+	if (!!roomWatchers[req.params.roomId]) {
+		roomWatchers[req.params.roomId].forEach(socketId => {
+			unsubscribeSocket(socketId, req.params.roomId);
+		});
 	}
 
-	rooms[req.params.roomId].subscribedSocketIds.forEach(socketId => {
-		unsubscribeSocket(socketId, req.params.roomId);
+	console.log(req.params.roomId);
+	deleteRoom(req.params.roomId, (err, result) => {
+		if (!!err) {
+			console.log(err.message);
+			res.status(500).send(err);//{successful: false, errMsg: 'Something weird happened'});
+			console.log('something happened... the delete was unsuccessful');
+			return;
+		}
+		if (result.result.n === 1) {
+			res.send({successful: true});
+			console.log('delete successful!');
+		} else if (result.result.n === 0) {
+			res.status(404).send({successful: false, errMsg: 'Room not found'});
+			console.log('they tried to delete a room that did not exist');
+		} else {
+			console.log(result.result);
+			res.status(500).send(err);//{successful: false, errMsg: 'Something weird happened'});
+			console.log('something happened... the delete was unsuccessful');
+		}
 	});
-
-	delete rooms[req.params.roomId];
-
-	res.send({successful: true});
-	console.log('delete successful!');
 });
 
 app.post('/room/:roomId/message', (req, res) => {
@@ -86,39 +188,44 @@ app.post('/room/:roomId/message', (req, res) => {
 		return res.status(422).send({ successful: false, errMsg: 'Invalid room id!'});
 	}
 
-	if (!rooms[req.params.roomId]) {
-		return res.status(404).send({successful: false, errMsg: 'A room with that id was not found!'})
-	}
-
-	const room = rooms[req.params.roomId];
-	if (!!room.accessPassword && room.accessPassword !== req.params.accessPassword) {
-		return res.status(401).send({successful: false, errMsg: 'Invalid password!'});
-	}
-
-	const messageId = room.messages.length;
-	const message = {
-		id: messageId,
-		sender: req.body.user,
-		message: req.body.message,
-		timeStamp: Math.floor(Date.now() / 1000),
-	};
-	room.messages.push(message);
-	room.subscribedSocketIds.forEach(socketId => {
-		try {
-			const socket = activeSockets.find(socket => socket.id === socketId).socket;
-			socket.send(JSON.stringify({update: {roomId: room.id, message}}));
-		} catch(e) {
-			console.log('Something went wrong notifying a subscriber!');
-			console.log(e);
+	getRoom(req.params.roomId, room => {
+		if (!room) {
+			return res.status(404).send({successful: false, errMsg: 'A room with that id was not found!'})
 		}
-	});
 
-	res.send({successful: true, messageId: messageId});
-	console.log('message received!');
+		if (!!room.accessPassword && room.accessPassword !== req.params.accessPassword) {
+			return res.status(401).send({successful: false, errMsg: 'Invalid password!'});
+		}
+
+		const messageId = room.messages.length;
+		const message = {
+			id: messageId,
+			sender: req.body.user,
+			message: req.body.message,
+			timeStamp: Math.floor(Date.now() / 1000),
+		};
+		room.messages.push(message);
+		if (!!roomWatchers[room.roomId]) {
+			roomWatchers[room.roomId].forEach(socketId => {
+				try {
+					const socket = activeSockets.find(socket => socket.id === socketId).socket;
+					socket.send(JSON.stringify({update: {roomId: room.roomId, message}}));
+				} catch(e) {
+					console.log('Something went wrong notifying a subscriber!');
+					console.log(e);
+				}
+			});
+		}
+
+		updateRoom(room.roomId, room, () => {
+			res.send({successful: true, messageId: messageId});
+			console.log('message received!');
+		});
+	});
 });
 
 app.get('/room/:roomId', (req, res) => {
-	console.log('user wanted to view chatroom')
+	console.log('user wanted to view chatroom');
 	if (!req.params.roomId) {
 		return res.status(422).send({ successful: false, errMsg: 'Invalid room id!'});
 	}
@@ -127,25 +234,26 @@ app.get('/room/:roomId', (req, res) => {
 		return res.status(422).send({successful: false, errMsg: 'Missing messageCount!'})
 	}
 
-	if (!rooms[req.params.roomId]) {
-		return res.status(404).send({successful: false, errMsg: 'A room with that id was not found!'})
-	}
+	getRoom(req.params.roomId, room => {
+		if (!room) {
+			return res.status(404).send({successful: false, errMsg: 'A room with that id was not found!'})
+		}
 
-	const room = rooms[req.params.roomId];
-	if (!!room.accessPassword && room.accessPassword !== req.params.accessPassword) {
-		return res.status(401).send({successful: false, errMsg: 'Invalid password!'});
-	}
+		if (!!room.accessPassword && room.accessPassword !== req.params.accessPassword) {
+			return res.status(401).send({successful: false, errMsg: 'Invalid password!'});
+		}
 
-	let messageOffsetIndex = room.messages.length ;
-	if (req.body.messageOffset) {
-		messageOffsetIndex = room.messages.findIndex((message) => message.id === req.body.messageOffset) + 1;
-	}
+		let messageOffsetIndex = room.messages.length ;
+		if (req.body.messageOffset) {
+			messageOffsetIndex = room.messages.findIndex((message) => message.id === req.body.messageOffset) + 1;
+		}
 
-	const start = Math.max(messageOffsetIndex - req.body.messageCount, 0);
-	const messages = room.messages.slice(start, messageOffsetIndex);
+		const start = Math.max(messageOffsetIndex - req.body.messageCount, 0);
+		const messages = room.messages.slice(start, messageOffsetIndex);
 
-	res.send({successful: true, name: room.name, description: room.description, messages: messages});
-	console.log('user was able to see chatroom');
+		res.send({successful: true, name: room.name, description: room.description, messages: messages});
+		console.log('user was able to see chatroom');
+	});
 });
 
 
@@ -161,20 +269,29 @@ app.ws('/', (socket, req) => {
 			const roomId = args.subscribe.roomId;
 			const accessPassword = args.subscribe.accessPassword;
 
-			const room = rooms[roomId];
-			if (!room) {
-				return socket.send(JSON.stringify({error: 'Room not found!'}));
-			}
+			getRoom(roomId, room => {
+				console.log('user subscribed to room', roomId, room)
+				if (!room) {
+					return socket.send(JSON.stringify({error: 'Room not found!'}));
+				}
 
-			if (!!room.accessPassword && room.accessPassword !== accessPassword) {
-				return socket.send(JSON.stringify({error: 'Invalid password for room ' + room.name}));
-			}
+				if (!!room.accessPassword && room.accessPassword !== accessPassword) {
+					return socket.send(JSON.stringify({error: 'Invalid password for room ' + room.name}));
+				}
 
-			room.subscribedSocketIds.push(socketId);
-			roomSubscriptions.push(room.id);
+				if (!roomWatchers[roomId]) {
+					roomWatchers[roomId] = [];
+				}
+
+				roomWatchers[roomId].push(socketId);
+				roomSubscriptions.push(room.roomId);
+
+				console.log({roomWatchers});
+				console.log({roomSubscriptions});
+			});
 		}
 		if (!!args.unsubscribe) {
-			unsubscribeSocket(socket, args.unsubscribe.roomId);
+			unsubscribeSocket(socketId, args.unsubscribe.roomId);
 		}
 	});
 	socket.onclose(() => {
@@ -187,25 +304,24 @@ app.ws('/', (socket, req) => {
 });
 
 function unsubscribeSocket(socketId, roomId) {
+	if (!!roomWatchers[roomId]) {
+		const subscriptionIndex = roomWatchers[roomId].findIndex(x => x === socketId);
+		if (subscriptionIndex != -1) {
+			roomWatchers[roomId].splice(subscriptionIndex, 1);
+		}
+	}
+
 	const socket = activeSockets.find(x => x.id === socketId);
-	if (!socket) {
-		return;
-	}
+	if (!!socket) {
+		const roomIndex = socket.roomSubscriptions.findIndex(roomSubscription => roomSubscription === roomId);
+		if (roomIndex != -1) {
+			socket.roomSubscriptions.splice(roomIndex, 1);
+		}
+	}	
+}
 
-	const room = rooms[roomId];
-	if (!room) {
-		return;
-	}
-
-	const subscriptionIndex = room.subscribedSocketIds.findIndex(x => x === socketId);
-	if (subscriptionIndex != -1) {
-		room.subscribedSocketIds.splice(subscriptionIndex, 1);
-	}
-
-	const roomIndex = socket.roomSubscriptions.findIndex(roomSubscription => roomSubscription === roomId);
-	if (roomIndex != -1) {
-		socket.roomSubscriptions.splice(roomIndex, 1);
-	}
+const serverSetup = () => {
+	console.log(`${serverName} listening on port ${port}!`);
 }
 
 var fs = require("fs");
@@ -216,7 +332,8 @@ try {
 	port = settings.port;
 	serverName = settings.serverName;
 	adminPassword = settings.adminPassword;
-	app.listen(port, () => console.log(`${serverName} listening on port ${port}!`));
+	connectionString = settings.connectionString;
+	app.listen(port, serverSetup);
 } catch (e) {
 	const readline = require('readline').createInterface({
 		input: process.stdin,
@@ -231,14 +348,17 @@ try {
 			serverName = inputServerName;
 			readline.question('What should the administrator password be? ', (inputAdminPassword) => {
 				adminPassword = inputAdminPassword;
-				readline.close();
+				readline.question('What should the connection string be? ', (inputConnectionString) => {
+					adminPassword = inputConnectionString;
+					readline.close();
 
-				const settings = {port: port, serverName: serverName, adminPassword: adminPassword};
-				fs.writeFileSync('jazz-settings.json', JSON.stringify(settings));
-				console.log('"jazz-settings.json" created automatically for you.');
-				console.log();
+					const settings = {port: port, serverName: serverName, adminPassword: adminPassword};
+					fs.writeFileSync('jazz-settings.json', JSON.stringify(settings));
+					console.log('"jazz-settings.json" created automatically for you.');
+					console.log();
 
-				app.listen(port, () => console.log(`${serverName} listening on port ${port}!`));
+					app.listen(port, serverSetup);
+				});
 			});
 		});
 	});
